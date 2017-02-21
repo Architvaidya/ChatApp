40742c62dc636fc1aab738b2c5b1e46d6c891340
Architvaidya <archit.vaidya@gmail.com>
2017-02-21 15:14:47 -0500

    Added code to add sender in the request

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 var socket = io();
-
+var name = getQueryVariable('name') || 'Anonymous';
+var cabin = getQueryVariable('cabin');
+console.log(name+' wants to join '+cabin);
 socket.on('connect', function () {
 	console.log('Conncted to socket.io server!');
 });
@@ -8,8 +10,10 @@ socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New message:');
 	console.log(message.text);
-
-	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
+	var $message = jQuery('.messages');
+	$message.append('<p><strong>' + message.name + ' '+ momentTimestamp.local().format('h:mm a') + ':'+message.text+'</strong><p>');
+	//$message.append('<p>' +message.text+ '</p>')
+	//jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
 });
 
 
@@ -21,7 +25,8 @@ $form.on('submit', function (event) {
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
-		text: $message.val()
+		text: $message.val(),
+		name: name
 	});
 
 	$message.val('');
